ba49feaa44a1c0d6fc90cd012a2f7638a0aaf186
mohit <msrajput8349@gmail.com>
2026-04-07 23:51:44 +0530

    Initial backend commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+
+@com.example.demo.SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
