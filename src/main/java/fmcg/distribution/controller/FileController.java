ae336f50429bc3a9987f4b3125acbd647827f19c
mohit singh rajput <119424794+mohitsinghrajput8349@users.noreply.github.com>
2026-04-12 14:05:02 +0530

    Update FileController.java

--- src/main/java/fmcg/distribution/controller/FileController.java
+++ src/main/java/fmcg/distribution/controller/FileController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/files")
+@RequestMapping("/files")
 public class FileController {
 
     @Autowired
